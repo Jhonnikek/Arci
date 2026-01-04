e45b3ce6c3aff346e0edc52c37f18ecebcef8604
Jhonnikek <jhonnierdiaz12@proton.me>
2026-01-03 19:15:43 -0500

    style(ui): refine battery component

--- app/ui/components/bettery.py
+++ app/ui/components/bettery.py
@@ -1,18 +1,18 @@
-from PySide6.QtWidgets import QWidget, QHBoxLayout, QPushButton, QSlider, QComboBox
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QSlider, QLabel
 from PySide6.QtCore import Qt
 
 class WidgetBattery(QWidget):
     def __init__(self):
         super().__init__()
 
-        layout = QHBoxLayout()
+        layout = QVBoxLayout()
 
+        self.battery_label = QLabel("Battery Charge Limit")
+        layout.addWidget(self.battery_label)
+        
         self.battery_slider = QSlider(Qt.Horizontal)
         self.battery_slider.setMinimum(0)
         self.battery_slider.setMaximum(100)
-
-        layout = QHBoxLayout()
         layout.addWidget(self.battery_slider)
 
         self.setLayout(layout)
-
