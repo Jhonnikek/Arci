18ce059b6935b93c0fe96a478a7d43646f6519c6
Jhonnikek <jhonnierdiaz12@proton.me>
2026-01-05 19:44:14 -0500

    refactor: clarify command function and parameter names
    Change the names of command functions and parameters to better reflect
    their actual behavior

--- app/core/commands.py
+++ app/core/commands.py
@@ -7,8 +7,17 @@ def run_command(command_list):
         except Exception as e:
             print(f"Error running command: {e}")
 
-def change_profile(profile):
+def change_power_profile(profile):
     run_command(["asusctl", "profile", "--profile-set", profile])
+
+def change_aura_mode(mode):
+    run_command(["asusctl", "aura", mode])
+
+def change_rgb_lvl(brightness):
+    run_command(["asusctl", "-k", brightness])
 
 def change_color(mode, color):
     run_command(["asusctl", "aura", mode, "-c", color])
+
+def change_battery_limit(limit):
+    run_command(["asusctl", "-c", limit])
