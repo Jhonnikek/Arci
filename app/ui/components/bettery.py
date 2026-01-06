6321e3a6d06f32f63227ed5ac1a534bb161b564f
Jhonnikek <jhonnierdiaz12@proton.me>
2026-01-05 19:47:13 -0500

    feat: add battery charge limit support
    Connect the battery slider to real-time UI updates and applythe selected
    charge limit using core command integration

--- app/ui/components/bettery.py
+++ app/ui/components/bettery.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QSlider, QLabel
 from PySide6.QtCore import Qt
+from core.commands import change_battery_limit
 
 class WidgetBattery(QWidget):
     def __init__(self):
@@ -9,10 +10,19 @@ class WidgetBattery(QWidget):
 
         self.battery_label = QLabel("Battery Charge Limit")
         layout.addWidget(self.battery_label)
-        
+
         self.battery_slider = QSlider(Qt.Horizontal)
         self.battery_slider.setMinimum(0)
         self.battery_slider.setMaximum(100)
         layout.addWidget(self.battery_slider)
+        self.battery_slider.valueChanged.connect(self.update_label)
+        self.battery_slider.sliderReleased.connect(self.set_battery_charge_limit)
 
         self.setLayout(layout)
+
+    def update_label(self, limit):
+        self.battery_label.setText(f"Battery Charge Limit: {limit}%")
+
+    def set_battery_charge_limit(self):
+        limit = self.battery_slider.value()
+        change_battery_limit(str(limit))
