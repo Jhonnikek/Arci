65ef196636240c4a57798753c9856e9ff4afbf4f
Jhonnikek <jhonnierdiaz12@proton.me>
2026-01-05 19:46:37 -0500

    refactor: improve naming clarity and event handling
    Rename widgets and methods for better clarity and consistency,align
    imports with updated core commands, and simplify eventhandling
    
    Also add visual feedback for color selection in the keyboard UI

--- app/ui/components/keyboard.py
+++ app/ui/components/keyboard.py
@@ -1,7 +1,7 @@
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QSlider, QComboBox,QColorDialog, QLabel
 
 from PySide6.QtCore import Qt
-from core.commands import change_color
+from core.commands import change_color, change_aura_mode, change_rgb_lvl
 
 class WidgetKeyboard(QWidget):
     def __init__(self):
@@ -13,28 +13,41 @@ class WidgetKeyboard(QWidget):
         self.keyboard_label = QLabel("Keyboard")
         layout.addWidget(self.keyboard_label)
 
-        self.keyboard = QComboBox()
-        self.keyboard.addItems(["Static", "Breathe", "rainbow-Cycle", "rainbow-Wave", "Pulse"])
-        keyboard_layout.addWidget(self.keyboard)
+        self.aura_mode_combobox = QComboBox()
+        self.aura_mode_combobox.addItems(["Static", "Breathe", "Rainbow-Cycle", "Pulse"])
+        self.aura_mode_combobox.currentTextChanged.connect(self.set_aura_mode)
+        keyboard_layout.addWidget(self.aura_mode_combobox)
 
-        self.keyboard_levels = ["off", "low", "med", "high"]
-        self.keyboard_slider = QSlider(Qt.Horizontal)
-        self.keyboard_slider.setMinimum(0)
-        self.keyboard_slider.setMaximum(3)
-        keyboard_layout.addWidget(self.keyboard_slider)
+        self.rgb_levels = ["off", "low", "med", "high"]
+        self.rgb_levels_slider = QSlider(Qt.Horizontal)
+        self.rgb_levels_slider.setMinimum(0)
+        self.rgb_levels_slider.setMaximum(3)
+        self.rgb_levels_slider.sliderReleased.connect(self.on_rgb_level_changed)
+        keyboard_layout.addWidget(self.rgb_levels_slider)
 
-        self.keyboard_color = QPushButton(" ")
-        self.keyboard_color.clicked.connect(self.show_color_dialog)
-        self.keyboard_color.setFixedSize(40, 40)
-        keyboard_layout.addWidget(self.keyboard_color)
+        self.color_btn = QPushButton(" ")
+        self.color_btn.clicked.connect(self.pick_color)
+        self.color_btn.setFixedSize(40, 40)
+        keyboard_layout.addWidget(self.color_btn)
 
         layout.addLayout(keyboard_layout)
         self.setLayout(layout)
 
-    def show_color_dialog(self):
+    def pick_color(self):
         color = QColorDialog.getColor()
-        mode = self.keyboard.currentText()
+        mode = self.aura_mode_combobox.currentText()
         if color.isValid():
             color_name = color.name()
+            btn_color = f"QPushButton {{ color: {color_name};}}"
+            self.color_btn.setStyleSheet(btn_color)
             color_name = color_name.lstrip("#")
             change_color(mode.lower(), color_name)
+
+    def set_aura_mode(self):
+        mode = self.aura_mode_combobox.currentText()
+        change_aura_mode(mode.lower())
+
+    def on_rgb_level_changed(self):
+        value = self.rgb_levels_slider.value()
+        level = self.rgb_levels[value]
+        change_rgb_lvl(level)
