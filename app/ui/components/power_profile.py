3e5aa36d37590ec9946ad1119ccce1129785290a
Jhonnikek <jhonnierdiaz12@proton.me>
2026-01-05 19:49:16 -0500

    refactor: align naming with core commands
    Update imports and rename widgets and slot methods to improveconsistency
    and better reflect their behavior

--- app/ui/components/power_profile.py
+++ app/ui/components/power_profile.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QLabel
-from core.commands import change_profile
+from core.commands import change_power_profile
 
 class WidgetPowerProfile(QWidget):
     def __init__(self):
@@ -8,26 +8,26 @@ class WidgetPowerProfile(QWidget):
         layout = QVBoxLayout()
         power_profile_layout = QHBoxLayout()
 
-        self.power_profile_label = QLabel("Mode: ")
+        self.power_profile_label = QLabel("Mode ")
         layout.addWidget(self.power_profile_label)
 
-        self.btn_low_power = QPushButton("Low Power")
-        self.btn_low_power.clicked.connect(self.power_profile)
-        power_profile_layout.addWidget(self.btn_low_power)
+        self.low_power_btn = QPushButton("Low Power")
+        self.low_power_btn.clicked.connect(self.set_power_profile)
+        power_profile_layout.addWidget(self.low_power_btn)
 
-        self.btn_balanced = QPushButton("Balanced")
-        self.btn_balanced.clicked.connect(self.power_profile)
-        power_profile_layout.addWidget(self.btn_balanced)
+        self.balanced_btn = QPushButton("Balanced")
+        self.balanced_btn.clicked.connect(self.set_power_profile)
+        power_profile_layout.addWidget(self.balanced_btn)
 
-        self.btn_performance = QPushButton("Performance")
-        self.btn_performance.clicked.connect(self.power_profile)
-        power_profile_layout.addWidget(self.btn_performance)
+        self.performance_btn = QPushButton("Performance")
+        self.performance_btn.clicked.connect(self.set_power_profile)
+        power_profile_layout.addWidget(self.performance_btn)
 
         layout.addLayout(power_profile_layout)
         self.setLayout(layout)
 
-    def power_profile(self):
-        btn = self.sender()
-        profile = btn.text()
-        change_profile(profile)
+    def set_power_profile(self):
+        profile_btn = self.sender()
+        profile = profile_btn.text()
+        change_power_profile(profile)
         self.power_profile_label.setText(f"Mode: {profile}")
