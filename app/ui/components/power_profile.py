c8676199cafa8da2f00ff4c337270de47a434a40
Jhonnikek <jhonnierdiaz12@proton.me>
2026-01-08 09:49:18 -0500

    feat: add support for ACPI platform profiles

--- app/ui/components/power_profile.py
+++ app/ui/components/power_profile.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QLabel
 from core.commands import change_power_profile
+from core.utils import get_energy_mode
 
 class WidgetPowerProfile(QWidget):
     def __init__(self):
@@ -11,9 +12,9 @@ class WidgetPowerProfile(QWidget):
         self.power_profile_label = QLabel("Mode ")
         layout.addWidget(self.power_profile_label)
 
-        self.quite_btn = QPushButton("Quiet")
-        self.quite_btn.clicked.connect(self.set_power_profile)
-        power_profile_layout.addWidget(self.quite_btn)
+        self.quiet_btn = QPushButton("Quiet")
+        self.quiet_btn.clicked.connect(self.set_power_profile)
+        power_profile_layout.addWidget(self.quiet_btn)
 
         self.balanced_btn = QPushButton("Balanced")
         self.balanced_btn.clicked.connect(self.set_power_profile)
@@ -25,6 +26,7 @@ class WidgetPowerProfile(QWidget):
 
         layout.addLayout(power_profile_layout)
         self.setLayout(layout)
+        self.power_profile_label.setText(f"Mode: {get_energy_mode()}")
 
     def set_power_profile(self):
         profile_btn = self.sender()
