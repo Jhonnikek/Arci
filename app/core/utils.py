e2c6c303eb98788409e23c5eb063a0803887592c
Jhonnikek <jhonnierdiaz12@proton.me>
2026-01-08 09:48:34 -0500

    feat: add functions to obtain energy profile and battery charge limit

---
+++ app/core/utils.py
@@ -0,0 +1,20 @@
+import os
+
+PLATFORM_PROFILE = "/sys/firmware/acpi/platform_profile"
+BATTERY_PATHS = [
+    "/sys/class/power_supply/BAT0/charge_control_end_threshold",
+    "/sys/class/power_supply/BAT1/charge_control_end_threshold",
+]
+
+def get_energy_mode():
+    if os.path.exists(PLATFORM_PROFILE):
+        with open(PLATFORM_PROFILE, "r") as f:
+            return f.read().strip()
+    return "Desconocido"
+
+def get_battery_limit():
+    for path in BATTERY_PATHS:
+        if os.path.exists(path):
+            with open(path, "r") as f:
+                return f.read().strip()
+    return "unknown"
